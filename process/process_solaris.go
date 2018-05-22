7edb80e4bf9f8f5e04ffdd0c0ddc7020e86082f5
Fazal Majid <majid@apsalar.com>
2018-05-22 14:24:05 -0700

    another rewrite of upstream references

--- process/process_solaris.go
+++ process/process_solaris.go
@@ -9,9 +9,9 @@ import (
 	"syscall"
 	"unsafe"
 
-	"github.com/shirou/gopsutil/cpu"
-	"github.com/shirou/gopsutil/internal/common"
-	"github.com/shirou/gopsutil/net"
+	"github.com/fazalmajid/gopsutil/cpu"
+	"github.com/fazalmajid/gopsutil/internal/common"
+	"github.com/fazalmajid/gopsutil/net"
 )
 
 type MemoryMapsStat struct {
